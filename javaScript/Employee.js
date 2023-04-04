93e0421c931072e660adccc2a690f89320a4d00f
SOURAV <devbhsourav@gmail.com>
2023-04-04 23:09:37 +0530

    [sourav]add : addEmployeeUi

--- javaScript/Employee.js
+++ javaScript/Employee.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
                 const abc = `<tr>
                       <td scope="row"> <img src=${data[i].profile} width="30px" border-radius: "50%"  alt="profileImg"> ${data[i].name}</td>
                         <td>${data[i].gender}</td>
-
-                         <td>                         <nobr
-                         style="width: 100%; background-color: #e5f3ac; border-radius: 25%; text-align: center; margin-left: 4px;">
-                         ${data[i].department}</nobr>
-                         </td>
-                          <td>&#8377; ${data[i].salary}</td>
-                          <td>${data[i].date }</td>
-                          <td><a href=""><i class="glyphicon glyphicon-trash"></i></a> &nbsp; &nbsp;<a href=""><i class="glyphicon glyphicon-pencil"></i></a></td>
+                        <td>                         
+                            <nobr style="width: 100%; background-color: #e5f3ac; border-radius: 25%; text-align: center; margin-left: 4px;">
+                                ${data[i].department}
+                            </nobr>
+                        </td>
+                        <td>&#8377; ${data[i].salary}</td>
+                        <td>${data[i].date}</td>
+                        <td><a href=""><i class="glyphicon glyphicon-trash"></i></a> &nbsp; &nbsp;<a href=""><i class="glyphicon glyphicon-pencil"></i></a></td>
                       </tr>`;
 
                 $(".tablebody").append(abc);
